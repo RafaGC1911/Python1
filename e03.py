bc03d9205aa466b28338b58fe2269323280610f4
Rafael Guijarro Campano <rafaelguijarro7@gmail.com>
2024-10-14 18:56:09 +0200

    Subir ejercicio 3

---
+++ e03.py
@@ -0,0 +1,10 @@
+#3. Escribe los números pares del 2 hasta un número que se pida por teclado previamente.
+n= int(input("Introduce un número: "))
+
+print (f"Los números pares del 2 hasta {n}: ")
+for i in range(2, n + 1, 2):
+    print(i)
+    
+
+
+
