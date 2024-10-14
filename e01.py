ddf714106c069b0a4bb985b4db81dc51b5f14339
Rafael Guijarro Campano <rafaelguijarro7@gmail.com>
2024-10-14 18:13:35 +0200

    subir ejercicio1

---
+++ e01.py
@@ -0,0 +1,6 @@
+#Escribe un programa que pregunte el nombre y después de que el usuario lo introduzca muestre por pantalla las cadenas
+#¡Hola <nombre>! - Me alegro de conocerle, <nombre>  donde <nombre> es el nombre que el usuario haya introducido.
+nombre= input ("Cual es tu nombre?: ")
+
+print (f"¡Hola {nombre}!")
+print (f"Me alegro de conocerte {nombre}")
